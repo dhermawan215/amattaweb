418a33d7cf2365d63320d371543641031d5eecf3
dhermawan215 <dhermawan215@gmail.com>
2022-09-18 14:55:00 +0700

    update file dashboard view

--- resources/views/frontlayouts/app.blade.php
+++ resources/views/frontlayouts/app.blade.php
@@ -31,21 +31,8 @@
     @include('frontlayouts.navbar')
     <!-- Navbar End -->
 
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent pl-0">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Product</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-
     <!-- Corousel -->
-    <div class="container">
+    <div class="container mt-3">
         <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
---
+++ resources/views/pages/dashboard.blade.php
@@ -0,0 +1,3 @@
+@extends('frontlayouts.app')
+@section('content')
+@endsection
